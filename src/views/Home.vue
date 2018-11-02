842c58e2ab51fffb7d8cc0019536f717977ebc6f
Chris Michael <chris.perez@upr.edu>
2018-11-02 17:03:06 -0400

    :sparkles: LogIn method added in Home component and vuetify plugin removed.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,32 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <button @click="logIn()">Log In</button>
   </div>
 </template>
 
 <script>
   export default {
     name: 'home',
+    data(){
+      return{
+        user:{
+          id: '',
+          accessToken: ''
+        }
+      }
+    },
+    methods:{
+      logIn(){
+        FB.login(function(response){
+          console.log(response);
+          if(response.status == "connected"){
+            this.user = response.authResponse.userID;
+            this.accessToken = response.authResponse.accessToken;
+          }
+        },{scope:'public_profile, email'});
+      }
+    }
   }
 </script>
+
