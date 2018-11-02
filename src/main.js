17d5d016a08976685df2ce416b03fb6ca369911d
Chris Michael <chris.perez@upr.edu>
2018-11-02 14:22:12 -0400

    :sparkles: Vuejs store and Facebook Authentication added to repo.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import store from './store/store'
 
 Vue.config.productionTip = false
+
+import { facebookAuth } from './FacebookAuth/index'
 
 new Vue({
   router,
   store,
+  created(){
+    facebookAuth();
+  },
   render: h => h(App)
 }).$mount('#app')
